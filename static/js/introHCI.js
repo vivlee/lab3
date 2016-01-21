9304303951ff2bb8dfe3da75c7f272c28fcd1b74
Vivian Lee <v8lee@ucsd.edu>
2016-01-21 11:45:35 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -47,7 +47,8 @@ function projectClick(e) {
     if (description.length == 0) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-       $(description).hide("<div class='project-description'><p>Description of the project.</p></div>");
+       $(description).toggle( "fast", function() {
+      });
     }
 
 }
